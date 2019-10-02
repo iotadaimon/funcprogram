9878a8a3d40755b9ace509756194e16d6885a9f4
Mykola Demianko <mykola@demianko.com>
2019-10-02 18:39:26 +0300

    Add l3

---
+++ l3/src/l3.hs
@@ -0,0 +1,27 @@
+-- Load as a module with ghci.
+
+simpson :: (Float -> Float) -> Float -> Float -> Int -> Float
+simpson f a b n = do
+                    let h = (b - a) / (fromIntegral n)
+                    h / 3 * sum (calcvalues f h a n)
+
+calcvalues :: (Float -> Float) -> Float -> Float -> Int -> [Float]
+calcvalues f h a n = do
+                        let ks = [1..n-1]
+                        let temp = calcvaluesr [] f a h ks
+                        f a : temp ++ [(f (a + (fromIntegral n) * h))]
+
+calcvaluesr :: [Float] -> (Float -> Float) -> Float -> Float -> [Int] -> [Float]
+calcvaluesr ys f a h ks = do
+                            if null ks
+                            then
+                                ys
+                            else
+                                do
+                                    let k = head ks
+                                    if k `mod` 2 == 1
+                                    then
+                                        calcvaluesr (ys ++ [4 * (f (a + (fromIntegral k) * h))]) f a h (tail ks)
+                                    else
+                                        calcvaluesr (ys ++ [2 * (f (a + (fromIntegral k) * h))]) f a h (tail ks)
+
